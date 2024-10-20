d606936bf8c38b6ac8df8e312b679c0d6afa32ea
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2024-10-20 08:13:16 +0500

    nothing

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    // <p className='bg-slate-900 text-slate-200 mx-4 sm:bg-yellow-400 lg:bg-green-600 xl:bg-blue-400'>Lorem 
+    // ipsum dolor sit amet consectetur adipisicing elit. Commodi dolorem amet mollitia laudantium consectetur exercitationem, facere repellat expedita, eligendi necessitatibus quaerat, quia praesentium deserunt ut ea tempora corrupti eaque. Perspiciatis.</p>
+    <p className='mx-1 bg-slate-500 sm:bg-slate-500 md:bg-orange-600 lg:bg-pink-400 xl:bg-orange-900'>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus earum dolorem qui ex deleniti tempora expedita ipsa ullam perferendis optio. Sit est temporibus, aspernatur nemo reprehenderit libero voluptatem qui harum?
+    </p>
+  )
+}
+
+export default Page
